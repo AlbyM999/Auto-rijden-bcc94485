da51ad24caefecf7688695f4e0df5c9d40e5275c
AlbyM999 <2094183@talnet.nl>
2019-09-30 17:10:45 +0200

    Uitwerking van de exercise

--- rijbewijs.php
+++ rijbewijs.php
@@ -2,7 +2,7 @@
 $A = 16.5;
 print("how old are you?\n");
 $age = (float) fgets(STDIN);
-if ($age > $A){
+if ($age >= $A){
     print("you can start your driveing lessons\n");
 } else {
     print("you are to young\n");
